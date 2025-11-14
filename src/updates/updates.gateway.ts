03c686c5f98474578bec2d3f494ea2c0219c85e8
Timur Turatbekov <timatur242005@gmail.com>
2025-11-14 08:43:18 +0600

    Fix broken port

--- src/updates/updates.gateway.ts
+++ src/updates/updates.gateway.ts
@@ -16,7 +16,7 @@ export class UpdatesGateway implements OnModuleInit {
   }
 
   onModuleInit() {
-    this.wss = new WebSocketServer({ port: 3001 });
+    this.wss = new WebSocketServer({ port: 3002 });
 
     this.wss.on('connection', async (ws: any, req: IncomingMessage) => {
       // Получаем токен из query параметров
